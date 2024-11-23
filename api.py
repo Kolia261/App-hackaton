7a98c2b0e6b94bad8c16bc89792ee5769def72a4
Kolia261 <bv606118@gmail.com>
2024-11-23 20:25:21 +0600

    second commit
    commit

--- api.py
+++ api.py
@@ -1,20 +1,29 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from typing import List, Optional
+import common
 
 app = FastAPI()
 
 class UserList(BaseModel):
     users: list[str]
 
-fake_db = {
-    '1234': ['Иван', 'Петр', 'Анна'],
-    '5678': ['Сергей', 'Ольга']
-}
+class Task(BaseModel):
+    title: str
+    completed: bool = False
+
+class UserTasksAndGoals(BaseModel):
+    tasks: List[Task] = []
+    goals: List[str] = []
+
+@app.get("/get_tasks_and_goals")
+async def get_tasks_and_goals():
+    return common.tasks_and_goals
 
 @app.get("/get_code/{code}")
 async def get_code(code: str):
-    if code in fake_db:
-        user_list = UserList(users=fake_db[code])
+    if code in common.fake_db:
+        user_list = UserList(users=common.fake_db[code])
         return user_list
     
     raise HTTPException(status_code=404, detail="Список не найден")
--- bot.py
+++ bot.py
@@ -1,64 +1,13 @@
-import telebot
-import string
-import random
-import sqlite3
 import os
+import random
+import string
+import sqlite3
+import telebot
 
 TOKEN = os.getenv('TOKEN')
-
 bot = telebot.TeleBot(TOKEN)
 
-tasks_and_goals = {}
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "Привет! Это панель управления для родителей. Доступны следующие команды:\n"
-                                      "/view_tasks - просмотреть список задач\n"
-                                      "/add_task - добавить новую задачу\n"
-                                      "/set_goal - установить цель для ребенка\n"
-                                      "/save_code - сохранить код входа для ребенка")
-
-@bot.message_handler(commands=['view_tasks'])
-def view_tasks(message):
-    user_id = message.from_user.id
-    if user_id in tasks_and_goals:
-        tasks = tasks_and_goals[user_id].get('tasks', [])
-        goals = tasks_and_goals[user_id].get('goals', [])
-        
-        response = f'Задачи: {", ".join(tasks)}\nЦели: {", ".join(goals)}'
-    else:
-        response = 'У вас нет ни задач, ни целей.'
-    
-    bot.send_message(message.chat.id, response)
-@bot.message_handler(commands=['add_task'])
-def add_task(message):
-    msg = bot.reply_to(message, 'Введите название задачи:')
-    bot.register_next_step_handler(msg, process_add_task)
-
-def process_add_task(message):
-    task = message.text.strip()
-    user_id = message.from_user.id
-    if user_id not in tasks_and_goals:
-        tasks_and_goals[user_id] = {'tasks': [], 'goals': []}
-    
-    tasks_and_goals[user_id]['tasks'].append(task)
-    bot.send_message(message.chat.id, f'Задача "{task}" успешно добавлена.')
-
-@bot.message_handler(commands=['set_goal'])
-def set_goal(message):
-    msg = bot.reply_to(message, 'Введите цель для ребенка:')
-    bot.register_next_step_handler(msg, process_set_goal)
-
-def process_set_goal(message):
-    goal = message.text.strip()
-    user_id = message.from_user.id
-    if user_id not in tasks_and_goals:
-        tasks_and_goals[user_id] = {'tasks': [], 'goals': []}
-    
-    tasks_and_goals[user_id]['goals'].append(goal)
-    bot.send_message(message.chat.id, f'Цель "{goal}" успешно установлена.')
-
-conn = sqlite3.connect('users.db')
+conn = sqlite3.connect('tasks_and_goals.db', check_same_thread=False)
 cursor = conn.cursor()
 
 cursor.execute('''
@@ -68,19 +17,113 @@ CREATE TABLE IF NOT EXISTS users (
     secret_code TEXT
 );
 ''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    task TEXT,
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS goals (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    goal TEXT,
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+''')
+
 conn.commit()
+
+ALLOWED_USER_ID = os.getenv("TG_ID")  
+print(ALLOWED_USER_ID)
+def is_allowed_user(user_id):
+    return str(user_id) == ALLOWED_USER_ID
+    
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    if not is_allowed_user(message.from_user.id):
+        bot.send_message(message.chat.id, "У вас нет доступа к этому боту.")
+        return
+    bot.send_message(message.chat.id, "Привет! Это панель управления для родителей. Доступны следующие команды:\n"
+                                      "/view_tasks - просмотреть список задач\n"
+                                      "/add_task - добавить новую задачу\n"
+                                      "/set_goal - установить цель для ребенка\n"
+                                      "/save_code - сохранить код входа для ребенка")
+
+@bot.message_handler(commands=['view_tasks'])
+def view_tasks(message):
+    if not is_allowed_user(message.from_user.id):
+        bot.send_message(message.chat.id, "У вас нет доступа к этому боту.")
+        return
+
+    user_id = message.from_user.id
+    cursor.execute("SELECT task FROM tasks WHERE user_id=?", (user_id,))
+    tasks = cursor.fetchall()
+
+    cursor.execute("SELECT goal FROM goals WHERE user_id=?", (user_id,))
+    goals = cursor.fetchall()
+
+    task_list = [task[0] for task in tasks]
+    goal_list = [goal[0] for goal in goals]
+
+    response = (
+    f'Задачи: {", ".join(task_list) if task_list else "Нет задач"}\n'
+    f'Цели: {", ".join(goal_list) if goal_list else "Нет целей"}'
+)
+    
+    bot.send_message(message.chat.id, response)
+
+@bot.message_handler(commands=['add_task'])
+def add_task(message):
+    if not is_allowed_user(message.from_user.id):
+        bot.send_message(message.chat.id, "У вас нет доступа к этому боту.")
+        return
+
+    msg = bot.reply_to(message, 'Введите название задачи:')
+    bot.register_next_step_handler(msg, process_add_task)
+
+def process_add_task(message):
+    task = message.text.strip()
+    user_id = message.from_user.id
+
+    cursor.execute("INSERT INTO tasks (user_id, task) VALUES (?, ?)", (user_id, task))
+    conn.commit()
+    
+    bot.send_message(message.chat.id, f'Задача "{task}" успешно добавлена.')
+
+@bot.message_handler(commands=['set_goal'])
+def set_goal(message):
+    if not is_allowed_user(message.from_user.id):
+        bot.send_message(message.chat.id, "У вас нет доступа к этому боту.")
+        return
+
+    msg = bot.reply_to(message, 'Введите цель для ребенка:')
+    bot.register_next_step_handler(msg, process_set_goal)
+
+def process_set_goal(message):
+    goal = message.text.strip()
+    user_id = message.from_user.id
+
+    cursor.execute("INSERT INTO goals (user_id, goal) VALUES (?, ?)", (user_id, goal))
+    conn.commit()
+
+    bot.send_message(message.chat.id, f'Цель "{goal}" успешно установлена.')
 
 def generate_random_code(length=6):
     characters = string.ascii_letters + string.digits
     code = ''.join(random.choice(characters) for _ in range(length))
     return code
-
 @bot.message_handler(commands=['save_code'])
 def save_code(message):
-    msg = bot.reply_to(message, 'Сейчас бот сгенерирует код:')
-    bot.register_next_step_handler(msg, process_save_code)
+    if not is_allowed_user(message.from_user.id):
+        bot.send_message(message.chat.id, "У вас нет доступа к этому боту.")
+        return
 
-def process_save_code(message):
     secret_code = generate_random_code()
     user_id = message.from_user.id
 
@@ -102,9 +145,13 @@ def process_save_code(message):
 
 @bot.message_handler(commands=['update_code'])
 def update_code(message):
-    new_secret_code = generate_random_code()
+    if not is_allowed_user(message.from_user.id):
+        bot.send_message(message.chat.id, "У вас нет доступа к этому боту.")
+        return
 
+    new_secret_code = generate_random_code()
     user_id = message.from_user.id
+
     cursor.execute(
         "UPDATE users SET secret_code=? WHERE user_id=?",
         (new_secret_code, user_id),
@@ -115,5 +162,6 @@ def update_code(message):
         message.chat.id, 
         f'Ваш код был успешно обновлен до: {new_secret_code}.',
     )
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     bot.polling(none_stop=True)

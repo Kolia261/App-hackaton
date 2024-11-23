3d9a1c6097a7d49f76519b6e8685e7eee1af09ee
Kolia261 <bv606118@gmail.com>
2024-11-23 20:56:30 +0600

    commit
    commit УРА BD РАБОТАЕТ

--- api.py
+++ api.py
@@ -1,12 +1,15 @@
+import sqlite3
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import List, Optional
-import common
+from typing import List
+
+conn = sqlite3.connect('tasks_and_goals.db', check_same_thread=False)
+cursor = conn.cursor()
 
 app = FastAPI()
 
 class UserList(BaseModel):
-    users: list[str]
+    users: List[int]
 
 class Task(BaseModel):
     title: str
@@ -16,14 +19,25 @@ class UserTasksAndGoals(BaseModel):
     tasks: List[Task] = []
     goals: List[str] = []
 
-@app.get("/get_tasks_and_goals")
-async def get_tasks_and_goals():
-    return common.tasks_and_goals
+@app.get("/get_tasks_and_goals/{user_id}", response_model=UserTasksAndGoals)
+async def get_tasks_and_goals(user_id: int):
+    cursor.execute("SELECT task FROM tasks WHERE user_id=?", (user_id,))
+    tasks = cursor.fetchall()
+    
+    cursor.execute("SELECT goal FROM goals WHERE user_id=?", (user_id,))
+    goals = cursor.fetchall()
+    
+    task_list = [Task(title=task[0], completed=False) for task in tasks]
+    goal_list = [goal[0] for goal in goals]
 
-@app.get("/get_code/{code}")
+    return UserTasksAndGoals(tasks=task_list, goals=goal_list)
+
+@app.get("/get_code/{code}", response_model=UserList)
 async def get_code(code: str):
-    if code in common.fake_db:
-        user_list = UserList(users=common.fake_db[code])
-        return user_list
+    cursor.execute("SELECT user_id FROM users WHERE secret_code=?", (code,))
+    user = cursor.fetchone()
+    
+    if user:
+        return UserList(users=[user[0]])
     
     raise HTTPException(status_code=404, detail="Список не найден")
